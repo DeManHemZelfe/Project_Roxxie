b656d4e1a361746e715d9c63bf74ea6c35db8288
Niels Martens <niels_martens@outlook.com>
2022-12-27 14:40:41 +0100

    test

--- website/src/App.js
+++ website/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Dit is de base
+          Dit is de mainbase
         </p>
         <a
           className="App-link"
